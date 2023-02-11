293c275afb2cf7b14517d7f58bf1bedf309a8963
reeveng <govaertreeven@gmail.com>
2023-02-11 23:47:33 +0100

    update and create svelte site

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logo from "./assets/images/logo.svg";
+  import Collections from "./lib/Collections.svelte";
+</script>
+
+<main>
+  <div class="part">
+    <img
+      class="pfp"
+      src={logo}
+      alt="a logo of a cod (a fish species), free to roam the world"
+    />
+  </div>
+  <h1 class="part">
+    Perfect<br />
+    imperfections
+  </h1>
+  <p class="part about">
+    Some things aren't meant to be perfect, yet they are perfectly imperfect.
+  </p>
+  <Collections />
+</main>
---
+++ src/lib/Collections.svelte
@@ -0,0 +1,57 @@
+<script>
+  // @ts-nocheck
+
+  const imageModules = {
+    ...import.meta.glob("../assets/static/*/*.JPG"),
+    ...import.meta.glob("../assets/static/*/*.jpg"),
+  };
+  let openSections = [];
+
+  let urls = {};
+  for (const modulePath in imageModules) {
+    imageModules[modulePath]().then(({ default: imageUrl }) => {
+      let urlParts = imageUrl.split("/");
+      let collectionName = urlParts[urlParts.length - 2];
+
+      // if the collectionName isn't yet present in urls, create an empty array
+      if (urls[collectionName] == undefined) {
+        urls[collectionName] = [];
+      }
+
+      // add url to the right collection
+      urls[collectionName].push(imageUrl);
+      urls[collectionName] = urls[collectionName];
+    });
+    urls = urls;
+  }
+
+  const openNewSection = (sectionName) => {
+    if (openSections.includes(sectionName)) {
+      openSections = openSections.filter((i) => i != sectionName);
+    } else {
+      openSections.push(sectionName);
+      openSections = openSections;
+    }
+  };
+</script>
+
+<h2 class="part">Collections</h2>
+<ul>
+  {#each Object.entries(urls) as [collectionName, pictures]}
+    <li>
+      <button on:click={openNewSection(collectionName)}>
+        {collectionName}
+      </button>
+
+      {#if openSections.includes(collectionName)}
+        <ul>
+          {#each pictures as picture}
+            <li>
+              <img src={picture} alt="" />
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </li>
+  {/each}
+</ul>
