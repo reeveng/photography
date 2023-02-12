06eae8896687f4a18fa10ea6dc823b8a14d80a07
reeveng <govaertreeven@gmail.com>
2023-02-12 11:13:46 +0100

    add socials to the bottom => link back to socials page

--- src/App.svelte
+++ src/App.svelte
@@ -20,5 +20,32 @@
   <p class="part about">
     Some things aren't meant to be perfect, yet they are perfectly imperfect.
   </p>
+
   <Collections />
+
+  <div class="socials">
+    <hr />
+    <div class="links part">
+      <a
+        href="https://www.pexels.com/@j-mad-65591688/"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+      >
+        <i class="fa-solid fa-link" />
+        <span class="sr-only">
+          Link to pexels, a site where I you can find more of my photos
+        </span>
+      </a>
+      <a
+        href="https://www.pexels.com/@j-mad-65591688/"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+      >
+        <i class="fa-solid fa-camera-retro" />
+        <span class="sr-only">
+          Link to pexels, a site where I you can find more of my photos
+        </span>
+      </a>
+    </div>
+  </div>
 </main>
